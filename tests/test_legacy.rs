d7a3b2c2be13d70e14c91401404bdf9d7c051c40
Armin Ronacher <armin.ronacher@active-4.com>
2019-08-28 02:16:06 +0200

    Rename legacy tests

--- tests/test_legacy.rs
+++ tests/test_legacy.rs
@@ -5,36 +5,36 @@ use insta::{
 use std::fmt;
 
 #[test]
-fn test_debug_vector() {
+fn test_legacy_debug_vector() {
     assert_debug_snapshot_matches!("debug_vector", vec![1, 2, 3]);
 }
 
 #[test]
-fn test_unnamed_debug_vector() {
+fn test_legacy_unnamed_debug_vector() {
     assert_debug_snapshot_matches!(vec![1, 2, 3]);
     assert_debug_snapshot_matches!(vec![1, 2, 3, 4]);
     assert_debug_snapshot_matches!(vec![1, 2, 3, 4, 5]);
 }
 
 #[test]
-fn test_yaml_vector() {
+fn test_legacy_yaml_vector() {
     assert_yaml_snapshot_matches!("yaml_vector", vec![1, 2, 3]);
 }
 
 #[test]
-fn test_unnamed_yaml_vector() {
+fn test_legacy_unnamed_yaml_vector() {
     assert_yaml_snapshot_matches!(vec![1, 2, 3]);
     assert_yaml_snapshot_matches!(vec![1, 2, 3, 4]);
     assert_yaml_snapshot_matches!(vec![1, 2, 3, 4, 5]);
 }
 
 #[test]
-fn test_json_vector() {
+fn test_legacy_json_vector() {
     assert_json_snapshot_matches!("json_vector", vec![1, 2, 3]);
 }
 
 #[test]
-fn test_unnamed_json_vector() {
+fn test_legacy_unnamed_json_vector() {
     assert_json_snapshot_matches!(vec![1, 2, 3]);
     assert_json_snapshot_matches!(vec![1, 2, 3, 4]);
     assert_json_snapshot_matches!(vec![1, 2, 3, 4, 5]);
@@ -49,13 +49,13 @@ impl fmt::Display for TestDisplay {
 }
 
 #[test]
-fn test_display() {
+fn test_legacy_display() {
     let td = TestDisplay;
     assert_display_snapshot_matches!("display", td);
 }
 
 #[test]
-fn test_unnamed_display() {
+fn test_legacy_unnamed_display() {
     let td = TestDisplay;
     assert_display_snapshot_matches!(td);
     assert_display_snapshot_matches!("whatever");
