31280c6d824354e5456ef67fecae2cc227c04cc4
Armin Ronacher <armin.ronacher@active-4.com>
2019-08-28 02:18:52 +0200

    Simplified legacy macros

--- src/legacy_macros.rs
+++ src/legacy_macros.rs
@@ -3,42 +3,14 @@
 #[doc(hidden)]
 #[deprecated(since = "0.6.0", note = "Replaced by assert_yaml_snapshot")]
 macro_rules! assert_serialized_snapshot {
-    ($value:expr, @$snapshot:literal) => {{
-        $crate::_assert_serialized_snapshot!($value, Yaml, @$snapshot);
-    }};
-    ($value:expr, {$($k:expr => $v:expr),*}, @$snapshot:literal) => {{
-        $crate::_assert_serialized_snapshot!($value, {$($k => $v),*}, Yaml, @$snapshot);
-    }};
-    ($name:expr, $value:expr) => {{
-        $crate::_assert_serialized_snapshot!($name, $value, Yaml);
-    }};
-    ($name:expr, $value:expr, {$($k:expr => $v:expr),*}) => {{
-        $crate::_assert_serialized_snapshot!($name, $value, {$($k => $v),*}, Yaml);
-    }};
+    ($($t:tt)*) => { $crate::assert_serialized_snapshot!($($t)*); }
 }
 
 /// Legacy alias for `assert_yaml_snapshot`.
 #[macro_export]
 #[deprecated(since = "0.11.0", note = "Replaced by assert_yaml_snapshot")]
 macro_rules! assert_yaml_snapshot_matches {
-    ($value:expr, @$snapshot:literal) => {{
-        $crate::assert_yaml_snapshot!($value, @$snapshot);
-    }};
-    ($value:expr, {$($k:expr => $v:expr),*}, @$snapshot:literal) => {{
-        $crate::assert_yaml_snapshot!($value, {$($k => $v),*}, @$snapshot);
-    }};
-    ($value:expr, {$($k:expr => $v:expr),*}) => {{
-        $crate::assert_yaml_snapshot!($value, {$($k => $v),*});
-    }};
-    ($name:expr, $value:expr) => {{
-        $crate::assert_yaml_snapshot!($name, $value);
-    }};
-    ($name:expr, $value:expr, {$($k:expr => $v:expr),*}) => {{
-        $crate::assert_yaml_snapshot!($name, $value, {$($k => $v),*});
-    }};
-    ($value:expr) => {{
-        $crate::assert_yaml_snapshot!($value);
-    }};
+    ($($t:tt)*) => { $crate::assert_yaml_snapshot!($($t)*); }
 }
 
 /// Legacy alias for `assert_ron_snapshot`.
@@ -46,24 +18,7 @@ macro_rules! assert_yaml_snapshot_matches {
 #[cfg(feature = "ron")]
 #[deprecated(since = "0.11.0", note = "Replaced by assert_ron_snapshot")]
 macro_rules! assert_ron_snapshot_matches {
-    ($value:expr, @$snapshot:literal) => {{
-        $crate::assert_ron_snapshot!($value, @$snapshot);
-    }};
-    ($value:expr, {$($k:expr => $v:expr),*}, @$snapshot:literal) => {{
-        $crate::assert_ron_snapshot!($value, {$($k => $v),*}, @$snapshot);
-    }};
-    ($value:expr, {$($k:expr => $v:expr),*}) => {{
-        $crate::assert_ron_snapshot!($value, {$($k => $v),*});
-    }};
-    ($name:expr, $value:expr) => {{
-        $crate::assert_ron_snapshot!($name, $value);
-    }};
-    ($name:expr, $value:expr, {$($k:expr => $v:expr),*}) => {{
-        $crate::assert_ron_snapshot!($name, $value, {$($k => $v),*});
-    }};
-    ($value:expr) => {{
-        $crate::assert_ron_snapshot!($value);
-    }};
+    ($($t:tt)*) => { $crate::assert_ron_snapshot!($($t)*); }
 }
 
 /// Legacy alias for `assert_ron_snapshot`.
@@ -72,109 +27,37 @@ macro_rules! assert_ron_snapshot_matches {
 #[doc(hidden)]
 #[deprecated(since = "0.11.0", note = "Replaced by assert_ron_snapshot")]
 macro_rules! assert_ron_snapshot_matches {
-    ($value:expr, @$snapshot:literal) => {{
+    ($($t:tt)*) => {
         compile_error!(
             "insta was compiled without ron support. Enable the ron feature to reactivate it."
         );
-    }};
-    ($value:expr, {$($k:expr => $v:expr),*}, @$snapshot:literal) => {{
-        compile_error!(
-            "insta was compiled without ron support. Enable the ron feature to reactivate it."
-        );
-    }};
-    ($value:expr, {$($k:expr => $v:expr),*}) => {{
-        compile_error!(
-            "insta was compiled without ron support. Enable the ron feature to reactivate it."
-        );
-    }};
-    ($name:expr, $value:expr) => {{
-        compile_error!(
-            "insta was compiled without ron support. Enable the ron feature to reactivate it."
-        );
-    }};
-    ($name:expr, $value:expr, {$($k:expr => $v:expr),*}) => {{
-        compile_error!(
-            "insta was compiled without ron support. Enable the ron feature to reactivate it."
-        );
-    }};
-    ($value:expr) => {{
-        compile_error!(
-            "insta was compiled without ron support. Enable the ron feature to reactivate it."
-        );
-    }};
+    };
 }
 
 /// Legacy alias for `assert_json_snapshot`.
 #[macro_export]
 #[deprecated(since = "0.11.0", note = "Replaced by assert_json_snapshot")]
 macro_rules! assert_json_snapshot_matches {
-    ($value:expr, @$snapshot:literal) => {{
-        $crate::assert_json_snapshot!($value, @$snapshot);
-    }};
-    ($value:expr, {$($k:expr => $v:expr),*}, @$snapshot:literal) => {{
-        $crate::assert_json_snapshot!($value, {$($k => $v),*}, @$snapshot);
-    }};
-    ($value:expr, {$($k:expr => $v:expr),*}) => {{
-        $crate::assert_json_snapshot!($value, {$($k => $v),*});
-    }};
-    ($name:expr, $value:expr) => {{
-        $crate::assert_json_snapshot!($name, $value);
-    }};
-    ($name:expr, $value:expr, {$($k:expr => $v:expr),*}) => {{
-        $crate::assert_json_snapshot!($name, $value, {$($k => $v),*});
-    }};
-    ($value:expr) => {{
-        $crate::assert_json_snapshot!($value);
-    }};
+    ($($t:tt)*) => { $crate::assert_json_snapshot!($($t)*); }
 }
 
 /// Legacy alias for `assert_debug_snapshot`.
 #[macro_export]
 #[deprecated(since = "0.11.0", note = "Replaced by assert_debug_snapshot")]
 macro_rules! assert_debug_snapshot_matches {
-    ($value:expr, @$snapshot:literal) => {{
-        $crate::assert_debug_snapshot!($value, @$snapshot);
-    }};
-    ($name:expr, $value:expr) => {{
-        $crate::assert_debug_snapshot!($name, $value);
-    }};
-    ($value:expr) => {{
-        $crate::assert_debug_snapshot!($value);
-    }};
+    ($($t:tt)*) => { $crate::assert_debug_snapshot!($($t)*); }
 }
 
 /// Legacy alias for `assert_display_snapshot`.
 #[macro_export]
 #[deprecated(since = "0.11.0", note = "Replaced by assert_display_snapshot")]
 macro_rules! assert_display_snapshot_matches {
-    ($value:expr, @$snapshot:literal) => {{
-        $crate::assert_display_snapshot!($value, @$snapshot);
-    }};
-    ($name:expr, $value:expr) => {{
-        $crate::assert_display_snapshot!($name, $value);
-    }};
-    ($value:expr) => {{
-        $crate::assert_display_snapshot!($value);
-    }};
+    ($($t:tt)*) => { $crate::assert_display_snapshot!($($t)*); }
 }
 
 /// Legacy alias for `assert_snapshot`.
 #[macro_export]
 #[deprecated(since = "0.11.0", note = "Replaced by assert_snapshot")]
 macro_rules! assert_snapshot_matches {
-    ($value:expr, @$snapshot:literal) => {
-        $crate::assert_snapshot!($value, @$snapshot);
-    };
-    ($value:expr, $debug_expr:expr, @$snapshot:literal) => {
-        $crate::assert_snapshot!($value, $debug_expr, @$snapshot);
-    };
-    ($name:expr, $value:expr) => {
-        $crate::assert_snapshot!($name, $value);
-    };
-    ($name:expr, $value:expr, $debug_expr:expr) => {
-        $crate::assert_snapshot!($name, $value, $debug_expr);
-    };
-    ($value:expr) => {
-        $crate::assert_snapshot!($value);
-    };
+    ($($t:tt)*) => { $crate::assert_snapshot!($($t)*); }
 }
