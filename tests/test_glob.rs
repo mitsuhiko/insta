f912be5486edf22bacb8cc5886d5cbd10f8c8283
Matt Kantor <the.matt.kantor@gmail.com>
2020-08-14 20:30:13 -0700

    Allow globs to follow links.

--- tests/test_glob.rs
+++ tests/test_glob.rs
@@ -7,3 +7,11 @@ fn test_basic_globbing() {
         insta::assert_json_snapshot!(&contents);
     });
 }
+
+#[test]
+fn test_globs_follow_links() {
+    insta::glob!("link-to-inputs/*.txt", |path| {
+        let contents = std::fs::read_to_string(path).unwrap();
+        insta::assert_json_snapshot!(&contents);
+    });
+}
