9826e00393ea75d69d8944bf7adf164540f0241a
Matt Kantor <the.matt.kantor@gmail.com>
2020-08-14 17:39:34 -0700

    Add a test for typo'd globs.

--- tests/test_glob.rs
+++ tests/test_glob.rs
@@ -7,3 +7,10 @@ fn test_basic_globbing() {
         insta::assert_json_snapshot!(&contents);
     });
 }
+
+#[test]
+fn test_that_should_probably_fail() {
+    // FIXME: Ideally this would fail.
+    let typod_glob = "inputz/*.txt";
+    insta::glob!(typod_glob, |_| assert!(false));
+}
