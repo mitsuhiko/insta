90f285ae72379039c1141f903461573b4a544f83
Armin Ronacher <armin.ronacher@active-4.com>
2020-11-15 13:09:04 +0100

    Added more tests for inline replacements

--- cargo-insta/integration-tests/test-input/test_basic_utf8_inline.rs
+++ cargo-insta/integration-tests/test-input/test_basic_utf8_inline.rs
@@ -3,3 +3,18 @@
 fn test_non_basic_plane() {
     /* an offset here ❄️ */ insta::assert_snapshot!("a 😀oeu", @"");
 }
+
+#[test]
+fn test_remove_existing_value() {
+    insta::assert_snapshot!("this is the new value", @"this is the old value");
+}
+
+#[test]
+fn test_remove_existing_value_multiline() {
+    insta::assert_snapshot!(
+        "this is the new value",
+        @"this is\
+        this is the old value\
+        it really is"
+    );
+}
