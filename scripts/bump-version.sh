8bb2fdf3b0fc4cacaf8f120dd583a22f0153c235
Armin Ronacher <armin.ronacher@active-4.com>
2020-11-15 13:50:40 +0100

    1.2.0

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -9,3 +9,4 @@ NEW_VERSION="${1}"
 echo "Bumping version: ${NEW_VERSION}"
 perl -pi -e "s/^version = \".*?\"/version = \"$NEW_VERSION\"/" Cargo.toml
 perl -pi -e "s/^(insta.*)?version = \".*?\"/\$1version = \"$NEW_VERSION\"/" cargo-insta/Cargo.toml
+cd cargo-insta; cargo check
