046f249788916232a894e7f7d9b6c20b6c3a3259
Armin Ronacher <armin.ronacher@active-4.com>
2021-01-02 00:57:36 +0100

    Allow fuzzy matches in vscode insta extension

--- vscode-insta/src/extension.ts
+++ vscode-insta/src/extension.ts
@@ -9,11 +9,12 @@ import {
   Location,
   Position,
   ProviderResult,
+  workspace,
   Uri,
 } from "vscode";
 
-const NAMED_SNAPSHOT_ASSERTION: RegExp = /(?:\binsta::)?(?:assert(?:_\w+)_snapshot!)\(\s*['"]([^'"]+)['"]/;
-const UNNAMED_SNAPSHOT_ASSERTION: RegExp = /(?:\binsta::)?(?:assert(?:_\w+)_snapshot!)\(/;
+const NAMED_SNAPSHOT_ASSERTION: RegExp = /(?:\binsta::)?(?:assert(?:_\w+)?_snapshot!)\(\s*['"]([^'"]+)['"]/;
+const UNNAMED_SNAPSHOT_ASSERTION: RegExp = /(?:\binsta::)?(?:assert(?:_\w+)?_snapshot!)\(/;
 const FUNCTION: RegExp = /\bfn\s+([\w]+)\s*\(/;
 const TEST_DECL: RegExp = /#\[test\]/;
 const FILENAME_PARTITION: RegExp = /^(.*)[/\\](.*?)\.rs$/;
@@ -22,6 +23,12 @@ const SNAPSHOT_FUNCTION_STRIP: RegExp = /^test_(.*?)$/;
 const RUST_SELECTOR: DocumentFilter = {
   scheme: "file",
   language: "rust",
+};
+
+type SnapshotMatch = {
+  snapshotName: string;
+  path: string;
+  localModuleName: string;
 };
 
 class SnapshotPathProvider implements DefinitionProvider {
@@ -33,7 +40,7 @@ class SnapshotPathProvider implements DefinitionProvider {
   private resolveNamedSnapshot(
     document: TextDocument,
     position: Position
-  ): Uri | null {
+  ): SnapshotMatch | null {
     const line =
       (position.line >= 1 ? document.lineAt(position.line - 1).text : "") +
       document.lineAt(position.line).text;
@@ -48,9 +55,8 @@ class SnapshotPathProvider implements DefinitionProvider {
       return null;
     }
     const path = fileNameMatch[1];
-    const moduleName = fileNameMatch[2];
-    const snapshotFile = `${path}/snapshots/${moduleName}__${snapshotName}.snap`;
-    return Uri.file(snapshotFile);
+    const localModuleName = fileNameMatch[2];
+    return { snapshotName, path, localModuleName };
   }
 
   /**
@@ -59,7 +65,7 @@ class SnapshotPathProvider implements DefinitionProvider {
   private resolveUnnamedSnapshot(
     document: TextDocument,
     position: Position
-  ): Uri | null {
+  ): SnapshotMatch | null {
     function unnamedSnapshotAt(lineno: number): boolean {
       const line = document.lineAt(lineno).text;
       return !!(
@@ -101,29 +107,62 @@ class SnapshotPathProvider implements DefinitionProvider {
       return null;
     }
 
-    const snapshotName = functionName.match(SNAPSHOT_FUNCTION_STRIP)![1];
+    const snapshotName = `${functionName.match(SNAPSHOT_FUNCTION_STRIP)![1]}${
+      snapshotNumber > 1 ? `-${snapshotNumber}` : ""
+    }`;
     const fileNameMatch = document.fileName.match(FILENAME_PARTITION);
     if (!fileNameMatch) {
       return null;
     }
 
     const path = fileNameMatch[1];
-    const moduleName = fileNameMatch[2];
-    const snapshotFile = `${path}/snapshots/${moduleName}__${snapshotName}${
-      snapshotNumber > 1 ? `-${snapshotNumber}` : ""
-    }.snap`;
-    return Uri.file(snapshotFile);
+    const localModuleName = fileNameMatch[2];
+    return { snapshotName, path, localModuleName };
   }
 
   public provideDefinition(
     document: TextDocument,
     position: Position,
-    _token: CancellationToken
+    token: CancellationToken
   ): ProviderResult<Definition> {
-    const snapshot =
+    const snapshotMatch =
       this.resolveNamedSnapshot(document, position) ||
       this.resolveUnnamedSnapshot(document, position);
-    return snapshot ? new Location(snapshot, new Position(0, 0)) : null;
+    if (!snapshotMatch) {
+      return null;
+    }
+
+    const getSearchPath = function (
+      mode: "exact" | "wildcard-prefix" | "wildcard-all"
+    ): string {
+      return workspace.asRelativePath(
+        `${snapshotMatch.path}/snapshots/${mode !== "exact" ? "*__" : ""}${
+          snapshotMatch.localModuleName
+        }${mode === "wildcard-all" ? "__*" : ""}__${
+          snapshotMatch.snapshotName
+        }.snap`
+      );
+    };
+
+    function findFiles(path: string): Thenable<Uri | null> {
+      return workspace
+        .findFiles(path, "", 1, token)
+        .then((results) => results[0] || null);
+    }
+
+    // we try to find the file in three passes:
+    // - exact matchin the snapshot folder.
+    // - with a wildcard module prefix (crate__foo__NAME__SNAP)
+    // - with a wildcard module prefix and suffix (crate__foo__NAME__tests__SNAP)
+    // This is needed since snapshots can be contained in submodules. Since
+    // getting the actual module name is tedious we just hope the match is
+    // unique.
+    return findFiles(getSearchPath("exact"))
+      .then((rv) => rv || findFiles(getSearchPath("wildcard-prefix")))
+      .then((rv) => rv || findFiles(getSearchPath("wildcard-all")))
+      .then((snapshot) =>
+        snapshot ? new Location(snapshot, new Position(0, 0)) : null
+      );
   }
 }
 
