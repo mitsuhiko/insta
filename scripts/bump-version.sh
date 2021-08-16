d518be384ed1750683f98bafff9ad498594901d7
Armin Ronacher <armin.ronacher@active-4.com>
2021-08-16 17:02:07 +0200

    Added changelog entry

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,9 +4,13 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 
-NEW_VERSION="${1}"
+OLD_VERSION="${1}"
+NEW_VERSION="${2}"
 
+echo "Current version: ${OLD_VERSION}"
 echo "Bumping version: ${NEW_VERSION}"
-perl -pi -e "s/^version = \".*?\"/version = \"$NEW_VERSION\"/" Cargo.toml
-perl -pi -e "s/^(insta.*)?version = \".*?\"/\$1version = \"$NEW_VERSION\"/" cargo-insta/Cargo.toml
-cd cargo-insta; cargo check
+
+find . -name Cargo.toml -type f -exec sed -i '' -e "1,/^version/ s/^version.*/version = \"${NEW_VERSION}\"/" {} \;
+find . -name package.json -type f -exec sed -i '' -e "1,/\"version\":/ s/\"version\": .*/\"version\": \"${NEW_VERSION}\",/" {} \;
+
+cargo update -p sentry-release-parser
