1a259465402df8d107ea0da391e8d3730ca13d1d
Armin Ronacher <armin.ronacher@active-4.com>
2021-08-16 17:37:07 +0200

    Remove craft

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,13 +4,9 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 
-OLD_VERSION="${1}"
-NEW_VERSION="${2}"
+NEW_VERSION="${1}"
 
-echo "Current version: ${OLD_VERSION}"
 echo "Bumping version: ${NEW_VERSION}"
-
-find . -name Cargo.toml -type f -exec sed -i '' -e "1,/^version/ s/^version.*/version = \"${NEW_VERSION}\"/" {} \;
-
-cargo update -p insta
-(cd cargo-insta; cargo update -p cargo-insta)
+perl -pi -e "s/^version = \".*?\"/version = \"$NEW_VERSION\"/" Cargo.toml
+perl -pi -e "s/^(insta.*)?version = \".*?\"/\$1version = \"$NEW_VERSION\"/" cargo-insta/Cargo.toml
+cd cargo-insta; cargo check
