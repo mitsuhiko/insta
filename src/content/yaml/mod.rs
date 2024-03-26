cb20655183c8afd407913f66f102f107ee8550fb
Armin Ronacher <armin.ronacher@active-4.com>
2024-03-26 10:43:17 +0100

    Try to vendor yaml

--- src/content/yaml/mod.rs
+++ src/content/yaml/mod.rs
@@ -1,11 +1,13 @@
+pub mod parser;
+
 use std::path::Path;
 
 use crate::content::{Content, Error};
 
-use yaml_rust::{yaml::Hash as YamlObj, Yaml as YamlValue};
+use crate::content::yaml::parser::{yaml::Hash as YamlObj, Yaml as YamlValue};
 
 pub fn parse_str(s: &str, filename: &Path) -> Result<Content, Error> {
-    let mut blobs = yaml_rust::YamlLoader::load_from_str(s)
+    let mut blobs = crate::content::yaml::parser::yaml::YamlLoader::load_from_str(s)
         .map_err(|_| Error::FailedParsingYaml(filename.to_path_buf()))?;
 
     match (blobs.pop(), blobs.pop()) {
@@ -48,7 +50,7 @@ pub fn to_string(content: &Content) -> String {
     let yaml_blob = to_yaml_value(content);
 
     let mut buf = String::new();
-    let mut emitter = yaml_rust::YamlEmitter::new(&mut buf);
+    let mut emitter = crate::content::yaml::parser::emitter::YamlEmitter::new(&mut buf);
     emitter.dump(&yaml_blob).unwrap();
 
     if !buf.ends_with('\n') {
---
+++ src/content/yaml/parser/mod.rs
@@ -0,0 +1,110 @@
+// Copyright 2015, Yuheng Chen. See the LICENSE file at the top-level
+// directory of this distribution.
+
+//! YAML 1.2 implementation in pure Rust.
+//!
+//! # Usage
+//!
+//! This crate is [on github](https://github.com/chyh1990/yaml-rust) and can be
+//! used by adding `yaml-rust` to the dependencies in your project's `Cargo.toml`.
+//!
+//! ```toml
+//! [dependencies]
+//! yaml-rust = "0.4"
+//! ```
+//!
+//! And this in your crate root:
+//!
+//! ```rust
+//! extern crate yaml_rust;
+//! ```
+//!
+//! Parse a string into `Vec<Yaml>` and then serialize it as a YAML string.
+//!
+//! # Examples
+//!
+//! ```
+//! use yaml_rust::{YamlLoader, YamlEmitter};
+//!
+//! let docs = YamlLoader::load_from_str("[1, 2, 3]").unwrap();
+//! let doc = &docs[0]; // select the first document
+//! assert_eq!(doc[0].as_i64().unwrap(), 1); // access elements by index
+//!
+//! let mut out_str = String::new();
+//! let mut emitter = YamlEmitter::new(&mut out_str);
+//! emitter.dump(doc).unwrap(); // dump the YAML object to a String
+//!
+//! ```
+
+pub mod emitter;
+pub mod parser;
+pub mod scanner;
+pub mod yaml;
+
+pub use self::yaml::Yaml;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::content::yaml::parser::emitter::YamlEmitter;
+    use crate::content::yaml::parser::scanner::ScanError;
+    use crate::content::yaml::parser::yaml::YamlLoader;
+
+    #[test]
+    fn test_api() {
+        let s = "
+# from yaml-cpp example
+- name: Ogre
+  position: [0, 5, 0]
+  powers:
+    - name: Club
+      damage: 10
+    - name: Fist
+      damage: 8
+- name: Dragon
+  position: [1, 0, 10]
+  powers:
+    - name: Fire Breath
+      damage: 25
+    - name: Claws
+      damage: 15
+- name: Wizard
+  position: [5, -3, 0]
+  powers:
+    - name: Acid Rain
+      damage: 50
+    - name: Staff
+      damage: 3
+";
+        let docs = YamlLoader::load_from_str(s).unwrap();
+        let doc = &docs[0];
+
+        assert_eq!(doc[0]["name"].as_str().unwrap(), "Ogre");
+
+        let mut writer = String::new();
+        {
+            let mut emitter = YamlEmitter::new(&mut writer);
+            emitter.dump(doc).unwrap();
+        }
+
+        assert!(!writer.is_empty());
+    }
+
+    fn try_fail(s: &str) -> Result<Vec<Yaml>, ScanError> {
+        let t = YamlLoader::load_from_str(s)?;
+        Ok(t)
+    }
+
+    #[test]
+    fn test_fail() {
+        let s = "
+# syntax error
+scalar
+key: [1, 2]]
+key1:a2
+";
+        assert!(YamlLoader::load_from_str(s).is_err());
+        assert!(try_fail(s).is_err());
+    }
+}
