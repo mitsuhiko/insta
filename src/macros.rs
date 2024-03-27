9bbf8fbf478cc0ecda4934511ef288427dc38e60
Andrew Gallant <jamslam@gmail.com>
2024-03-27 16:49:53 -0400

    Change std macro calls to be fully qualified
    This changes calls like `format!` and `vec!` within insta's macros to be
    `std::format!` and `std::vec!`. I ran into this issue in a crate where
    std's prelude isn't enabled, even in tests. While perhaps a bit of a
    niche case, I don't think this fix has any downsides.
    
    The specific kind of error that happens is if you use
    `insta::assert_snapshot!` (or similar) and the (for example) `format!`
    macro isn't in scope. Here's a small example. First a `Cargo.toml`:
    
    ```toml
    [package]
    publish = false
    name = "insta-no-std"
    version = "0.1.0"
    edition = "2021"
    
    [dev-dependencies]
    insta = "1.37.0"
    
    [lib]
    name = "insta_no_std"
    path = "lib.rs"
    ```
    
    And now `lib.rs`:
    
    ```rust
    
    extern crate std;
    
    fn itworks() {
        insta::assert_snapshot!((-128i8).saturating_abs());
    }
    ```
    
    And now here's what happens when you try to build it:
    
    ```
    $ cargo t --no-run
       Compiling insta-no-std v0.1.0 (/home/andrew/tmp/scratch/rust/insta-no-std)
    error: cannot find macro `format` in this scope
     --> lib.rs:8:5
      |
    8 |     insta::assert_snapshot!((-128i8).saturating_abs());
      |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      |
      = note: this error originates in the macro `insta::assert_snapshot` (in Nightly builds, run with -Z macro-backtrace for more info)
    help: consider importing this macro
      |
    3 + use std::format;
      |
    
    error: could not compile `insta-no-std` (lib test) due to 1 previous error
    ```
    
    This can be pretty easily worked around by adding `use std::format;` so
    that the macro can call it. But I figure it's better to just have the
    macros do the right thing by default. Moreover, I suspect this is also
    important in other (perhaps strange) contexts where there is a custom
    `format!` macro defined and in scope. `insta` would use the custom macro
    instead of the one from `std`. But with this change, it will always use
    the one from `std`.

--- src/macros.rs
+++ src/macros.rs
@@ -277,7 +277,7 @@ macro_rules! _assert_serialized_snapshot {
 macro_rules! _prepare_snapshot_for_redaction {
     ($value:expr, {$($k:expr => $v:expr),*}, $format:ident, $location:ident) => {
         {
-            let vec = vec![
+            let vec = std::vec![
                 $((
                     $crate::_macro_support::Selector::parse($k).unwrap(),
                     $crate::_macro_support::Redaction::from($v)
@@ -313,7 +313,7 @@ macro_rules! _prepare_snapshot_for_redaction {
 #[macro_export]
 macro_rules! assert_debug_snapshot {
     ($($arg:tt)*) => {
-        $crate::_assert_snapshot_base!(transform=|v| format!("{:#?}", v), $($arg)*)
+        $crate::_assert_snapshot_base!(transform=|v| std::format!("{:#?}", v), $($arg)*)
     };
 }
 
@@ -392,7 +392,7 @@ macro_rules! assert_display_snapshot {
 #[macro_export]
 macro_rules! assert_snapshot {
     ($($arg:tt)*) => {
-        $crate::_assert_snapshot_base!(transform=|v| format!("{}", v), $($arg)*)
+        $crate::_assert_snapshot_base!(transform=|v| std::format!("{}", v), $($arg)*)
     };
 }
 
