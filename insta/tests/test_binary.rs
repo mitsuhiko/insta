5f8de35934b0c11ff8417d4cceffe85350a3ed3c
Florian Plattner <florian@lasernoises.com>
2024-05-02 15:32:49 +0200

    Add more tests for file extension of binary snapshots

--- insta/tests/test_binary.rs
+++ insta/tests/test_binary.rs
@@ -6,3 +6,19 @@ fn test_binary_snapshot() {
         file.write_all(&[0, 1, 2, 3]).unwrap();
     });
 }
+
+#[test]
+fn test_file_extension_collision() {
+    // ubuntu snap packages also have the .snap extension so let's make sure that doesn't cause
+    // problems
+    insta::assert_binary_snapshot!("snap", &mut |file| {
+        file.write_all(&[0, 1, 0, 1]).unwrap();
+    });
+}
+
+#[test]
+fn test_file_empty_extension() {
+    insta::assert_binary_snapshot!("", &mut |file| {
+        file.write_all(&[1, 3, 3, 7]).unwrap();
+    });
+}
