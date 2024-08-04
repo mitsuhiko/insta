31008f96e3624b37861a7e871f11ff827494afbb
a-kenji <aks.kenji@protonmail.com>
2024-08-04 19:09:14 +0200

    Update snapshot file
    This updates a test snapshot.

--- insta/src/utils.rs
+++ insta/src/utils.rs
@@ -111,7 +111,7 @@ pub fn format_rust_expression(value: &str) -> Cow<'_, str> {
 #[test]
 fn test_format_rust_expression() {
     use crate::assert_snapshot;
-    assert_snapshot!(format_rust_expression("vec![1,2,3]"), @"vec![1, 2, 3]");
+    assert_snapshot!(format_rust_expression("vec![1,2,3]"), @"vec![1,2,3]");
     assert_snapshot!(format_rust_expression("vec![1,2,3].iter()"), @"vec![1, 2, 3].iter()");
     assert_snapshot!(format_rust_expression(r#"    "aoeu""#), @r###""aoeu""###);
     assert_snapshot!(format_rust_expression(r#"  "aoe😄""#), @r###""aoe😄""###);
