d045b10157d6f191de2a8bd47bd71679c8abfa0a
Maximilian Roos <m@maxroos.com>
2024-09-11 11:21:54 -0700

    Add test for reporting correct number of snapshots
    This was fixed in 1.40, added a test out of #593 to confirm

--- cargo-insta/tests/main.rs
+++ cargo-insta/tests/main.rs
@@ -484,11 +484,12 @@ fn test_root() {
 }
 
 /// Check that in a workspace with a default root crate, running `cargo insta
-/// test --workspace` will update snapsnots in both the root crate and the
+/// test --workspace --accept` will update snapsnots in both the root crate and the
 /// member crate.
 #[test]
-fn test_root_crate_all() {
-    let test_project = workspace_with_root_crate("root-crate-all".to_string()).create_project();
+fn test_root_crate_workspace_accept() {
+    let test_project =
+        workspace_with_root_crate("root-crate-workspace-accept".to_string()).create_project();
 
     let output = test_project
         .cmd()
@@ -510,16 +511,33 @@ fn test_root_crate_all() {
          member/src
            member/src/lib.rs
     +      member/src/snapshots
-    +        member/src/snapshots/root_crate_all_member__member.snap
+    +        member/src/snapshots/root_crate_workspace_accept_member__member.snap
        src
          src/main.rs
     +    src/snapshots
-    +      src/snapshots/root_crate_all__root.snap
+    +      src/snapshots/root_crate_workspace_accept__root.snap
     "###     );
 }
 
 /// Check that in a workspace with a default root crate, running `cargo insta
-/// test` will only update snapsnots in the root crate
+/// test --workspace` will correctly report the number of pending snapshots
+#[test]
+fn test_root_crate_workspace() {
+    let test_project =
+        workspace_with_root_crate("root-crate-workspace".to_string()).create_project();
+
+    let output = test_project
+        .cmd()
+        .args(["test", "--workspace"])
+        .output()
+        .unwrap();
+
+    // 1.39 had a bug where it would claim there were 3 snapshots here
+    assert!(String::from_utf8_lossy(&output.stderr).contains("info: 2 snapshots to review"));
+}
+
+/// Check that in a workspace with a default root crate, running `cargo insta
+/// test --accept` will only update snapsnots in the root crate
 #[test]
 fn test_root_crate_no_all() {
     let test_project = workspace_with_root_crate("root-crate-no-all".to_string()).create_project();
@@ -619,8 +637,8 @@ fn test_member_2() {
         )
 }
 
-/// Check that in a workspace with a virtual manifest, running `cargo insta test --workspace`
-/// will update snapshots in all member crates.
+/// Check that in a workspace with a virtual manifest, running `cargo insta test
+/// --workspace --accept` updates snapshots in all member crates.
 #[test]
 fn test_virtual_manifest_all() {
     let test_project =
@@ -656,8 +674,8 @@ fn test_virtual_manifest_all() {
     "###     );
 }
 
-/// Check that in a workspace with a virtual manifest, running `cargo insta test`
-/// updates snapshots in all member crates.
+/// Check that in a workspace with a virtual manifest, running `cargo insta test
+/// --accept` updates snapshots in all member crates.
 #[test]
 fn test_virtual_manifest_default() {
     let test_project =
