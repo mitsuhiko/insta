85dd8f99e06b1d4aefeb4fb8c163336edd5fc20b
Florian Plattner <florian@lasernoises.com>
2024-09-23 00:23:19 +0200

    Add more basic tests for binary snapshots

--- insta/tests/test_binary.rs
+++ insta/tests/test_binary.rs
@@ -2,3 +2,20 @@
 fn test_binary_snapshot() {
     insta::assert_binary_snapshot!("txt", b"test".to_vec());
 }
+
+#[test]
+#[should_panic(expected = "this file extension is not allowed")]
+fn test_new_extension() {
+    insta::assert_binary_snapshot!("new", b"test".to_vec());
+}
+
+#[test]
+#[should_panic(expected = "file extensions starting with 'new.' are not allowed")]
+fn test_extension_starting_with_new() {
+    insta::assert_binary_snapshot!("new.gz", b"test".to_vec());
+}
+
+#[test]
+fn test_multipart_extension() {
+    insta::assert_binary_snapshot!("tar.gz", b"test".to_vec());
+}
