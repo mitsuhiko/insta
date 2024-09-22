caa1e8f96c8ef4e33c028d0f24c4e4fa36d8e45f
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2024-09-22 12:12:15 -0700

    Consolidate a couple of test files (#608)
    Not as extreme as https://github.com/mitsuhiko/insta/pull/459, but
    consolidates a few files that were separate binaries and 1-2 functions.

--- insta/tests/test_basic.rs
+++ insta/tests/test_basic.rs
@@ -116,3 +116,22 @@ fn insta_sort_order() {
         insta::assert_yaml_snapshot!(m);
     });
 }
+
+#[test]
+fn test_crlf() {
+    insta::assert_snapshot!("foo\r\nbar\r\nbaz");
+}
+
+#[test]
+fn test_trailing_crlf() {
+    insta::assert_snapshot!("foo\r\nbar\r\nbaz\r\n");
+}
+
+#[test]
+fn test_trailing_crlf_inline() {
+    insta::assert_snapshot!("foo\r\nbar\r\nbaz\r\n", @r#"
+    foo
+    bar
+    baz
+    "#);
+}
