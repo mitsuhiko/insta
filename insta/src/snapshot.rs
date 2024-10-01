24290a17a5fe0597e657c7c8ca81a8004aa8ff92
Maximilian Roos <m@maxroos.com>
2024-10-01 10:18:37 -0700

    Improve error message on file write error
    Will help to diagnose #621

--- insta/src/snapshot.rs
+++ insta/src/snapshot.rs
@@ -491,7 +491,8 @@ impl Snapshot {
         }
 
         let serialized_snapshot = self.serialize_snapshot(md);
-        fs::write(path, serialized_snapshot)?;
+        fs::write(path, serialized_snapshot)
+            .map_err(|e| content::Error::FileIo(e, path.to_path_buf()))?;
         Ok(())
     }
 
