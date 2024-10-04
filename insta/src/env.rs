379fbe44427cae3a998342a07816eac0828b826e
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2024-10-03 23:08:56 -0700

    Warn if extension is passed with a leading dot (#636)
    If someone passes `.snap` as an extension, they'll get files of
    `foo..snap`, which is probably not what they want.
    
    We could strip the leading `.`
    
    Also some light refactoring

--- insta/src/env.rs
+++ insta/src/env.rs
@@ -20,7 +20,11 @@ pub fn get_tool_config(workspace_dir: &Path) -> Arc<ToolConfig> {
         .lock()
         .unwrap()
         .entry(workspace_dir.to_path_buf())
-        .or_insert_with(|| ToolConfig::from_workspace(workspace_dir).unwrap().into())
+        .or_insert_with(|| {
+            ToolConfig::from_workspace(workspace_dir)
+                .unwrap_or_else(|e| panic!("Error building config from {:?}: {}", workspace_dir, e))
+                .into()
+        })
         .clone()
 }
 
@@ -96,7 +100,7 @@ impl std::error::Error for Error {
 }
 
 /// Represents a tool configuration.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ToolConfig {
     force_pass: bool,
     require_full_match: bool,
