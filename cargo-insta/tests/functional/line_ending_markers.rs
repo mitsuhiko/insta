4c55a458b61332f649dfb5fd53bf8e89f8035ff7
Maximilian Roos <m@maxroos.com>
2025-09-10 18:43:03 -0700

    Fix Windows path issue in test fixture
    Replace backslashes with forward slashes in CARGO_MANIFEST_DIR path
    to avoid TOML parsing errors on Windows

--- cargo-insta/tests/functional/line_ending_markers.rs
+++ cargo-insta/tests/functional/line_ending_markers.rs
@@ -639,7 +639,7 @@ serde = { version = "1.0", features = ["derive"] }
 
 [dev-dependencies]
 "#
-            .replace("$PROJECT_PATH", env!("CARGO_MANIFEST_DIR"))
+            .replace("$PROJECT_PATH", &env!("CARGO_MANIFEST_DIR").replace('\\', "/"))
             .replace("cargo-insta", "insta"),
         )
         .create_project();
